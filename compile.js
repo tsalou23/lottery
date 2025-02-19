325bbd8ad51e617ba37064b9321addd94625bc61
Giorgos Tsaloukidis <itsalou23@gmail.com>
2025-02-20 00:13:01 +0200

    new compile

--- compile.js
+++ compile.js
@@ -4,5 +4,24 @@ const solc = require("solc");
 
 const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
 const source = fs.readFileSync(lotteryPath, "utf8");
-
-module.exports = solc.compile(source, 1).contracts[":Lottery"];
+const input = {
+    language: 'Solidity',
+    sources: {
+      'Lottery.sol': {
+        content: source
+      }
+    },
+    settings: {
+      outputSelection: {
+        '*': {
+          '*': ['abi', 'evm.bytecode', 'evm.deployedBytecode']
+        }
+      }
+    }
+  };
+  
+  // Compile the contract
+  const output = JSON.parse(solc.compile(JSON.stringify(input)));
+  
+  // Export the compiled contract
+  module.exports = output.contracts['Lottery.sol'].Lottery;
