1c1194bb7dae33d976deecbfc0c192cee5155f9b
tsalou23 <itsalou23@gmail.com>
2025-02-19 23:26:14 +0200

    updated code

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,34 +1,33 @@
-pragma solidity ^0.4.17;
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.19;
 
 contract Lottery {
     address public manager;
-    address[] public players;
-    
-    function Lottery() public {
+    address [] public players;
+
+    constructor() {
         manager = msg.sender;
     }
-    
     function enter() public payable {
         require(msg.value > .01 ether);
         players.push(msg.sender);
     }
-    
-    function random() private view returns (uint) {
-        return uint(keccak256(block.difficulty, now, players));
-    }
-    
+   function random() private view returns (uint) {
+    return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, players, msg.sender)));
+}
+
     function pickWinner() public restricted {
+        require(players.length >0 ,"No players in the lottery");
         uint index = random() % players.length;
-        players[index].transfer(this.balance);
+        address winner = players[index];
+        uint prize = address(this).balance;
+        (bool succes, )=payable(winner).call{value: prize}("");
+        require(succes, "Transfer failed");
         players = new address[](0);
     }
-    
     modifier restricted() {
-        require(msg.sender == manager);
+        require(msg.sender == manager,"Only manager can call this");
         _;
     }
-    
-    function getPlayers() public view returns (address[]) {
-        return players;
-    }
-}   
+}
